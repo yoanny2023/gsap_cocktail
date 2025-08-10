ec2b287f8e48c4a6588cd8073e906b3e9af09791
yoanny <manuelvaal2021@gmail.com>
2025-08-10 12:32:20 +0100

    implement hero scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,13 @@
 import { useGSAP } from '@gsap/react'
 import { SplitText } from 'gsap/all'
 import gsap from 'gsap'
-import React from 'react'
+import { useRef } from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 const Hero = () => {
+  const videoRef = useRef();
+  const isMobile = useMediaQuery({maxWidth:767});
+
   useGSAP(()=>{
     const heroSplit = new SplitText(".title",{type:"chars,words"});
     const paragraphSplit = new SplitText(".subtitle",{type:"lines"});
@@ -37,6 +41,26 @@ const Hero = () => {
     })
     .to(".right-leaf",{y: 200},0)
     .to(".left-leaf",{y: -200},0)
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    // create the timeline with a default duration
+    const tl = gsap.timeline({
+      scrollTrigger:{
+        trigger:"video",
+        start: startValue,
+        end: endValue,
+        scrub:4,
+        pin:true,
+      }
+    })
+    
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current,{
+      currentTime: videoRef.current.duration
+      })
+    }
   },[]);
   
   return (
@@ -67,6 +91,16 @@ const Hero = () => {
           </div>
         </div>
       </section>
+      <div className="video absolute inset-0">
+        <video 
+        ref={videoRef}
+        src="/videos/input.mp4"
+        muted
+        playsInline
+        preload='auto'
+        />
+
+      </div>
     </>
   )
 }
